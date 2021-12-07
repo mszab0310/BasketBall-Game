a6bd68a5eca4d777e33d183a952c6c6eaee963d4
mszab0310 <mszabi0310@gmail.com>
2021-12-07 15:25:05 +0200

    Added trigger collider inside hoop for score detection

--- Basketball/Assets/Scripts/Ball.cs
+++ Basketball/Assets/Scripts/Ball.cs
@@ -15,41 +15,20 @@ public class Ball : MonoBehaviour
     private SpriteRenderer _spriteRenderer;
     private Vector3 _lastVelocity;
     private bool delay = false;
+    public GameObject hoop;
 
-    private int _inputSize = 0;
-    private Vector2[] _directions = new Vector2[100];
-    private float[] _launchForces = new float[100];
+
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        SetLaunch();
     }
     // Start is called before the first frame update
     void Start()
     {
         _startPosition = _rigidbody2D.position;
         _rigidbody2D.isKinematic = true;
-
-        int i = 0;
-        /*while(i < _inputSize)
-        {
-            if(delay == false)
-            {
-                Debug.LogError("ball nr " + i);
-                LaunchBall(_directions[i], _launchForces[i]);
-                i++;
-            }
-        }*/
-        LaunchBall(_directions[i], _launchForces[i]);
-        /*for(int i = 0; i < _inputSize; i++)
-        {
-            Debug.LogError("ball nr " + i);
-            StartCoroutine(DelayOnStart());
-            //Thread.Sleep(3000);
-            LaunchBall(_directions[i], _launchForces[i]);
-        }*/
     }
 
     private IEnumerator DelayOnStart()
@@ -57,7 +36,7 @@ public class Ball : MonoBehaviour
         yield return new WaitForSeconds(5);
     }
 
-    private void NotOnMouseUp()
+    private void OnMouseUp()
     {
         Vector2 currentPosition = _rigidbody2D.position;
         Vector2 direction = _startPosition - currentPosition;
@@ -66,17 +45,15 @@ public class Ball : MonoBehaviour
         _rigidbody2D.constraints = RigidbodyConstraints2D.None;
         _rigidbody2D.AddForce(direction * _launchForce);
         delay = true;
-        //StartCoroutine(ResetAfterTimeEnds());
-
     }
 
-    private void NotOnMouseDrag()
+    private void OnMouseDrag()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 desiredPosition = mousePosition;
 
         float distance = Vector2.Distance(desiredPosition, _startPosition);
-        if(distance > _maxDragDistance)
+        if (distance > _maxDragDistance)
         {
             Vector2 direction = desiredPosition - _startPosition;
             direction.Normalize();
@@ -88,26 +65,32 @@ public class Ball : MonoBehaviour
     void Update()
     {
         _lastVelocity = _rigidbody2D.velocity;
-        
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.LogError("OnCollisionEnter");
-        //var speed = _lastVelocity.magnitude;
-        //var direction = Vector3.Reflect(_lastVelocity.normalized, collision.contacts[0].normal);
-        //_rigidbody2D.velocity = direction * speed / (float)1.5;
         if (delay == true)
         {
             delay = false;
-            Debug.LogWarning("Delay started");
             StartCoroutine(ResetAfterDelay());
         }
+
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject == hoop && _rigidbody2D.position.y > collision.gameObject.transform.position.y)
+        {
+            Debug.Log("Oy cunt" + _rigidbody2D.position.y +" " + collision.gameObject.transform.position.y);
+        }
+        
+    }
+
 
     private IEnumerator ResetAfterDelay()
     {
         yield return new WaitForSeconds(5);
-        Debug.LogError("Reset started");
         _rigidbody2D.position = _startPosition;
         _rigidbody2D.isKinematic = true;
         _rigidbody2D.velocity = Vector2.zero;
@@ -116,34 +99,6 @@ public class Ball : MonoBehaviour
         delay = false;
     }
 
-    private void SetLaunch()
-    {
-        ReadTextFile("C:/Users/buciu/Desktop/input.txt");
-    }
-
-    private void ReadTextFile(string filePath)
-    {
-        //Vector2 direction;
-        //float launchForce;
-        StreamReader streamReader = new StreamReader(filePath);
-        while (!streamReader.EndOfStream)
-        {
-            string input = streamReader.ReadLine();
-            string[] words = input.Split(' ');
-            _directions[_inputSize].x = (float)System.Convert.ToDouble(words[0]);
-            _directions[_inputSize].y = (float)System.Convert.ToDouble(words[1]);
-            _launchForces[_inputSize] = (float)System.Convert.ToDouble(words[2]);
-            Debug.Log("x: " + _directions[_inputSize].x + " y: " + _directions[_inputSize].y + " force: "
-                + _launchForces[_inputSize]);
-            _inputSize++;
-            Debug.LogWarning(" inputSize: " + _inputSize);
-            /*direction.x = System.Convert.ToInt32(words[0]);
-            direction.y = System.Convert.ToInt32(words[1]);
-            launchForce = (float)System.Convert.ToDouble(words[2]);*/
-
-        }
-        streamReader.Close();
-    }
 
     private void LaunchBall(Vector2 _direction, float _launchForce)
     {
